b863fd551e9612843f1256f60444f50caa4ad925
Tsukasa Arima <pyar6329@gmail.com>
2019-11-12 21:32:04 +0900

    remove sql case

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,6 @@ set -e
 case "$1" in
   "sh" | "ash" | "bash" | "/bin/sh" )
     exec "/bin/sh";;
-  *\.sql )
-    exec /usr/local/bundle/bin/sqlint "$@";;
   * )
     exec find "${WORKSPACE}" -name "*.sql" | xargs -I {} -n1 /usr/local/bundle/bin/sqlint {};;
 esac
